60d34afb46e82d86e706b8f0001638ed0286b0f5
Francis Deoferio <francis.deoferio@gmail.com>
2016-11-05 21:14:20 +0800

    Server with incorporated sockets

--- src/ServerClass.java
+++ src/ServerClass.java
@@ -5,41 +5,57 @@ import javax.imageio.*;
 import java.awt.event.*;
 
 public class ServerClass extends JFrame implements Runnable, ActionListener {
+	private String portstr;
+    private String playersstr;
+    private int portint;
+    private int playerint;
+
+	private ChatServer chatServer;
+
+	private Background portBG;            //home panel
     private Background homeBG;            //home panel
     private Background createdBG;         //exit server creation panel
 
     private JPanel front;               //base panel
-    private JTextField input;
+    private JTextField portinput;
+    private JTextField playerinput;
 
+    private JButton nextButton;
     private JButton createButton;
     private JButton exitButton;
-    private JButton exitButton2;
 
     private CardLayout cards;
 
     // Component backgrounds
+    private Image portImage;
     private Image homeImage;
     private Image createdImage;
+
+    private Image nextButtonImage;
     private Image createButtonImage;
     private Image exitButtonImage;
 
     public ServerClass() {
+    	portBG = new Background();
         homeBG = new Background();
         createdBG = new Background();
 
         front = new JPanel();
-        input = new JTextField();
+        portinput = new JTextField();
+        playerinput = new JTextField();
 
         cards = new CardLayout();
 
         //START MENU
         front.setLayout(cards);
+        front.add(portBG, "portBG");
         front.add(homeBG, "homeBG");
         front.add(createdBG, "createdBG");
         front.setPreferredSize(new Dimension(1250, 800));
 
         readImages();
     
+    	portBG.setLayout(null);
         homeBG.setLayout(null);
         createdBG.setLayout(null);     
 
@@ -56,67 +72,99 @@ public class ServerClass extends JFrame implements Runnable, ActionListener {
     private void readImages() {
         try {
             // Panel Backgrounds
+            portImage = ImageIO.read(this.getClass().getResource("res/portImage.png"));
             homeImage = ImageIO.read(this.getClass().getResource("res/homeImage.png"));
             createdImage = ImageIO.read(this.getClass().getResource("res/createdImage.png"));
 
             // Button Backgrounds
+            nextButtonImage = ImageIO.read(this.getClass().getResource("res/nextButtonImage.png"));
             createButtonImage = ImageIO.read(this.getClass().getResource("res/createButtonImage.png"));
             exitButtonImage = ImageIO.read(this.getClass().getResource("res/exitButtonImage.png"));
+
         } catch(IOException ioe) {
             System.out.println("Error while reading image file.");
         }
 
+        portBG.setImage(new ImageIcon(portImage));
         homeBG.setImage(new ImageIcon(homeImage));
         createdBG.setImage(new ImageIcon(createdImage));
     }
 
     // Load and set buttons
     private void loadButtons() {
+    	nextButton = new JButton(new ImageIcon(createButtonImage));
         createButton = new JButton(new ImageIcon(createButtonImage));
         exitButton = new JButton(new ImageIcon(exitButtonImage));
-        exitButton2 = new JButton(new ImageIcon(exitButtonImage));
 
+        nextButton.addActionListener(this);
         createButton.addActionListener(this);
         exitButton.addActionListener(this);
-        exitButton2.addActionListener(this);
 
+        nextButton.setSize(300, 150);
         createButton.setSize(300, 150);
         exitButton.setSize(300, 150);
-        exitButton2.setSize(300, 150);
-        input.setSize(300, 75);
 
+        portinput.setSize(300, 75);
+        playerinput.setSize(300, 75);
+
+        nextButton.setLocation(480, 600);
         createButton.setLocation(480, 600);
-        //exitButton.setLocation(625, 600);
-        exitButton2.setLocation(480, 600);
-        input.setLocation(475, 380);
+        exitButton.setLocation(480, 600);
 
+        portinput.setLocation(475, 380);
+        playerinput.setLocation(475, 380);
+
+        portBG.add(nextButton);
+        portBG.add(portinput);
         homeBG.add(createButton);
-        //homeBG.add(exitButton);
-        homeBG.add(input);
-        createdBG.add(exitButton2);
+        homeBG.add(playerinput);
+        createdBG.add(exitButton);
     }
 
     // Handle mouse clicks
     public void actionPerformed(ActionEvent e){
-        if (e.getSource() == createButton) {
-            String playersstr = input.getText();
-            
+
+    	//try catch for port
+    	if(e.getSource() == nextButton){    		
+    		portstr = portinput.getText();
             try{
-                int playerint = Integer.parseInt(playersstr);
-                if(playerint > 0){
-                    cards.show(front, "createdBG");
-                    
+            	portint = Integer.parseInt(portstr);
+
+            	if(portint >= 1024 && portint <= 65535){
+            		cards.show(front, "homeBG");
                 }
                 else{
-                    JOptionPane.showMessageDialog(this, "Input must be greater than 0");
-                    input.setText("");
+                    JOptionPane.showMessageDialog(this, "Port can be any number from 1024 to 65535");
+                    portinput.setText("");
+                    return;
+                }
+
+            } catch (Exception excp){
+                JOptionPane.showMessageDialog(this, "Port must be a whole number");
+                portinput.setText("");
+                return;
+            }
+    	}
+
+    	//try catch for number of players
+        if (e.getSource() == createButton) {
+            playersstr = playerinput.getText();
+            try{
+                playerint = Integer.parseInt(playersstr);
+                if(playerint >= 3){
+                    cards.show(front, "createdBG");
+                    chatServer = new ChatServer(portint, playerint);
+                }
+                else{
+                    JOptionPane.showMessageDialog(this, "Input must be greater than or equal to 3");
+                    playerinput.setText("");
                 }
             } catch (Exception excp){
                 JOptionPane.showMessageDialog(this, "Input must be a whole number");
-                input.setText("");
+                playerinput.setText("");
             }
             
-        } else if (e.getSource() == exitButton || e.getSource() == exitButton2) {
+        } else if (e.getSource() == exitButton) {
             System.exit(0);
         }
     }
